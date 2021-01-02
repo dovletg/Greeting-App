d561450d34f16605ee00b4d3f701b1090572a882
jahan <jahan@j-MacBook-Air.home>
2021-01-02 16:34:52 -0500

    Add event listener and handler

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: 'Guest'
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      userName: e.target.value
+    })
+  }
+
   render () {
   return (
     <div>
-      <h1>Hello Guest</h1>
+      <h1>Hello {this.state.userName}</h1>
       <label htmlFor="user_name">Enter your name:</label><br/>
-      <input id="user_name" type="text"/>
+      <input id="user_name" type="text" onInput={this.handleChanhe}/>
     </div>
   );
+ }
 }
 
 export default App;
