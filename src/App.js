ed3e9d717cf69751e52efcc72c85720cd14213fb
jahan <jahan@j-MacBook-Air.home>
2021-01-02 15:55:37 -0500

    Render a component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 //import logo from './logo.svg';
 //import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component {
+  render () {
   return (
     <div>
       <h1>Hello Guest</h1>
